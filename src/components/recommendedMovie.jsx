aa6fc042b61afccbb6a2b359deeb9391e514fcd1
Srushtipatel14 <srushtip579@gmail.com>
2025-03-30 17:39:01 +0530

    30 mar

--- src/components/recommendedMovie.jsx
+++ src/components/recommendedMovie.jsx
@@ -1,51 +1,3 @@
-// import "../css/recommended.css";
-// import { IoIosArrowForward } from "react-icons/io";
-// import { FaStar } from "react-icons/fa";
-// import { Card } from "react-bootstrap";
-
-// const RecommendedMovie = () => {
-//   return (
-//     <div className="container-fluid p-0">
-//       <div className="movie_wrapper mx-auto">
-//         <div className="d-flex justify-content-between mb-2">
-//           <p className="title_font">Recommended Movies</p>
-//           <p className="text_font d-flex align-items-center gap-1">
-//             See All <IoIosArrowForward />
-//           </p>
-//         </div>
-
-//         <div className="d-flex align-items-center flex-wrap">
-//           {[1, 2, 3, 4, 5,6,7].map((item) => (
-//             <Card key={item} className="movie-card p-0">
-//               <div style={{ height: "350px" }}>
-//                 <img
-//                   variant="top"
-//                   src={`https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrwFBFgTscQ8nz7a0Vi3BbA5OU0M4Wuu7itw&s`}
-//                   style={{
-//                     width: "95%",
-//                     height: "100%",
-//                     objectFit: "cover",
-//                     borderRadius:"10px"
-//                   }}
-//                 />
-//               </div>
-//               <div className="rating_card">
-//                 <p className="m-0 d-flex align-items-center fs-5">
-//                   <FaStar size={20} color="red" />
-//                   8/10 · 37.3K Votes
-//                 </p>
-//               </div>
-//             </Card>
-//           ))}
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default RecommendedMovie;
-
-
 import "../css/recommended.css";
 import { IoIosArrowForward } from "react-icons/io";
 import { FaStar } from "react-icons/fa";
@@ -55,7 +7,7 @@ const RecommendedMovie = () => {
   return (
     <div className="container-fluid p-0">
       <div className="movie_wrapper mx-auto">
-        <div className="d-flex justify-content-between mb-2">
+        <div className="d-flex justify-content-between mb-2 first_movie_sec">
           <p className="title_font">Recommended Movies</p>
           <p className="text_font d-flex align-items-center gap-1">
             See All <IoIosArrowForward />
