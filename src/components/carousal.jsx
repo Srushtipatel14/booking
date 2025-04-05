28bcaf126c05c67d7ffda3f959cf95689e3d6560
Srushtipatel14 <srushtip579@gmail.com>
2025-04-05 21:33:37 +0530

    today

--- src/components/carousal.jsx
+++ src/components/carousal.jsx
@@ -24,7 +24,7 @@ const Carousel = () => {
         }}
         modules={[Autoplay, Pagination, EffectFade]}
       >
-        <SwiperSlide>
+        {/* <SwiperSlide>
           <div className="carousel-slide carousel-slide1">
             <div className='left_carousel1 d-sm-block p-sm-3 w-100'>
               <div className="d-flex align-items-center gap-1 mb-4 d-none d-md-flex">
@@ -35,36 +35,35 @@ const Carousel = () => {
                 />
                 <p
                   className="m-0 fs-4 "
-                  style={{ lineHeight: "1", position: "relative", top: "-3px",color:"#FFFFFF" }}
+                  style={{ lineHeight: "1", position: "relative", top: "-3px", color: "#FFFFFF" }}
                 >
                   Bookzy
                 </p>
               </div>
               <div className='display-5'>
-                <p className='left_carousel1_text'>GET READY FOR MOVIE DATE WITH <span style={{color:"#d78519"}}>YOUR LOVED ONCE</span></p>
+                <p className='left_carousel1_text'>GET READY FOR MOVIE DATE WITH <span style={{ color: "#d78519" }}>YOUR LOVED ONCE</span></p>
               </div>
               <div>
                 <button className='left_carousel1_btn'>Book Tickets</button>
               </div>
             </div>
             <div className='right_carousel1 d-none d-md-block'>
-                <img src="ticket.png" alt="ticket_image" style={{height:"350px",maxWidth:"330px",width:"300px"}}/>  
+              <img src="ticket.png" alt="ticket_image" style={{ height: "350px", maxWidth: "330px", width: "300px" }} />
             </div>
           </div>
-        </SwiperSlide>
+        </SwiperSlide> */}
         <SwiperSlide>
           <div className="carousel-slide carousel-slide2">
-          <div className='left_carousel2 d-sm-block p-sm-3 w-100'>
+            <div className='left_carousel2 d-sm-block p-sm-3 w-100'>
             </div>
             <div className='right_carousel2 d-none d-md-block'>
             </div>
           </div>
         </SwiperSlide>
-        <SwiperSlide>
+        {/* <SwiperSlide>
           <div className="carousel-slide carousel-slide3">
-            
           </div>
-        </SwiperSlide>
+        </SwiperSlide> */}
       </Swiper>
     </div>
   );
