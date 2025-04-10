503b05a8c75141f1a726d5a11e9c613cf3ccad00
Srushtipatel14 <srushtip579@gmail.com>
2025-04-10 14:45:06 +0530

    edit profile screen done

--- src/components/editProfile.jsx
+++ src/components/editProfile.jsx
@@ -2,21 +2,93 @@ import "../css/editprofile.css";
 const EditProfile = () => {
     return (
         <div className="container-fluid edit_profile m-0 py-3">
-            <div className="acc_detail">
-                <div className="acc_detail1">
-                    <img
-                        src="user.png"
-                        alt="user"
-                        className="edit_image position-relative"
-                    />
-                </div>
-                <div className="acc_detail2 p-4">
-                <div className="fs-4 fw-bold">Account Details</div>
-                <div className="d-flex py-3 gap-5">
-                    <div>Email Address</div>
-                    <div className="px-5">srushtip579@gamil.com</div>
-                </div>
-                </div>
+            <div className="edit_detail">
+                <form>
+                    <section className="acc_detail my-3">
+                        <div className="acc_detail1 px-3">
+                            <img
+                                src="user.png"
+                                alt="user"
+                                className="edit_image position-relative"
+                            />
+                        </div>
+                        <div className="acc_detail2 px-5 py-2">
+                            <div className="fs-4 edit_pro_header my-3">Account Details</div>
+                            <div className="d-flex py-3">
+                                <div className="lable_name">Email Address</div>
+                                <div className="input_name">srushtip579@gamil.com</div>
+                            </div>
+                            <div className="d-flex align-items-center mb-3 py-3">
+                                <div className="lable_name">Mobile Number</div>
+                                <div className="input_name">
+                                    <input type="text" placeholder="+Add Mobile Number" className="form-control phone_number"/>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+
+                    <section className="per_detail my-3">
+                        <div className="per_detail2 px-5 py-2">
+                            <div className="fs-4 edit_pro_header my-3">Personal Details</div>
+                            <div className="d-flex mb-3 py-2">
+                                <div className="lable_name">User Name</div>
+                                <div className="input_name">
+                                    <input placeholder="Enter user name here" type="text" className="form-control" />
+                                </div>
+                            </div>
+                            <div className="d-flex mb-3 py-2">
+                                <div className="lable_name">First Name</div>
+                                <div className="input_name">
+                                    <input placeholder="Enter first name here" type="text" className="form-control" />
+                                </div>
+                            </div>
+                            <div className="d-flex mb-3 py-2">
+                                <div className="lable_name">Last Name</div>
+                                <div className="input_name">
+                                    <input placeholder="Enter last name here" type="text" className="form-control" />
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+
+                    <section className="per_detail my-3">
+                        <div className="per_detail2 px-5 py-2">
+                            <div className="fs-4 edit_pro_header my-3">Address (Optional)</div>
+
+                            <div className="d-flex mb-3 py-2">
+                                <div className="lable_name">Pincode</div>
+                                <div className="input_name">
+                                    <input type="text" className="form-control" />
+                                </div>
+                            </div>
+                            <div className="d-flex mb-3 py-2">
+                                <div className="lable_name">Street</div>
+                                <div className="input_name">
+                                    <input type="text" className="form-control" />
+                                </div>
+                            </div>
+                            <div className="d-flex mb-3 py-2">
+                                <div className="lable_name">City</div>
+                                <div className="input_name">
+                                    <input type="text" className="form-control" />
+                                </div>
+                            </div>
+                            <div className="d-flex mb-3 py-2">
+                                <div className="lable_name">State</div>
+                                <div className="input_name">
+                                    <input type="text" className="form-control" />
+                                </div>
+                            </div>
+                            <div className="d-flex mb-3 py-2">
+                                <div className="lable_name">Country</div>
+                                <div className="input_name">
+                                    <input type="text" className="form-control" />
+                                </div>
+                            </div>
+
+                        </div>
+                    </section>
+                </form>
             </div>
         </div>
     )
